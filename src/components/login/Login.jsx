c8af4e0224d5832826810e261afac184d2783ac5
saudkhanbpk <saudkhanbpk@gmail.com>
2022-10-11 12:49:02 +0500

    icon log with fb text added

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -8,15 +8,19 @@ const Login = () => {
           <div className='col-6'>
 
           </div>
-          <div className='col-6'>
+          <div className='col-6 border'>
             <div className='logindiv'>
             <div className='wraper'>
-              <h2 className='fw-bold'>Log In</h2>
-              <div className='logininputdiv'>
+            <div className='logininputdiv'>
+            <h2 className='fw-bold d-flex justify-content-center'>Log In</h2>
+            <div className='bg-light'>
+              <span >Icon</span>
+              <span>Log in with facebook</span>
+            </div>
                 <input type="email" className='inputlogin' placeholder='Email' />
                 <input type="password" className='inputlogin' placeholder='Password' />
-                <button className='btn btn-primary'>Log In</button>
-                <a href=''>Forget Password</a>
+                <button className='btn btn-primary m-auto btlog fs-5'>Log In</button>
+                <a href='' className=' forget'>Forget Password?</a>
                 </div>
               </div>
             </div>
