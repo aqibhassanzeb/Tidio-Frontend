6b9814643e6ba93c04f0ecaabde0a691b0a5c9fc
devshehzadshah <devshehzadshah249@gmail.com>
2022-10-11 10:15:48 +0500

    navbar done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,43 +6,22 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import "../Header/Header.css"
 function Header() {
   return (
-    <div className='container-fluid'>
-        <div className='row py-4 '>
-         <div className='col-sm-3  d-flex align-items-center'>
-         <Navbar.Brand >logo</Navbar.Brand>
-         </div>
-         <div className='col-sm-6 home_link'>
-         <Nav className="">
-         <NavDropdown title="Product" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Learning" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+    <div className='container-fluid sticky-nav px-4'>
+      <div className='row py-4 '>
+        <div className='col-sm-3  d-flex align-items-center'>
+          <Navbar.Brand >logo</Navbar.Brand>
+        </div>
+        <div className='col-sm-6 home_link'>
+          <Nav className="">
+            <Nav.Link href="#link" className='home'>Home</Nav.Link>
+            <Nav.Link href="#link" className='home' >Pricing</Nav.Link>
           </Nav>
-         </div>
+        </div>
         <div className='col-sm-3 register'>
-        <a className='d-flex align-items-center logout'>Logout</a> &nbsp;
-        <button type="button" class="btn btn-light px-3">Register</button>
+          <a className='d-flex align-items-center logout'>Logout</a> &nbsp;&nbsp;
+          <button type="button" class="btn reg_padding">Register</button>
         </div>
-        </div>
+      </div>
     </div>
   )
 }
