feec9bc364349c1a2069fbc79cb04d9f55a640d4
saudkhanbpk <saudkhanbpk@gmail.com>
2022-10-12 10:29:16 +0500

    login and registration ui fixed

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -8,46 +8,47 @@ import { userLogin } from '../../apis/Auth-api';
 
 
 const Login = () => {
-  const navigate =useNavigate()
+  const navigate = useNavigate()
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [loader, setLoader] = useState(false)
   const [error, setError] = useState(false)
-  const handleLogin =()=>{
-    if(!email && !password){
+  const handleLogin = () => {
+    if (!email && !password) {
       return toast.error("please fill the fields");
-  }
-  setLoader(true)
-  const payload={email,password}
-  userLogin(payload).then((res)=>{
-      localStorage.setItem("token",res.data.token)
-      localStorage.setItem("user",JSON.stringify(res.data.user))
+    }
+    setLoader(true)
+    const payload = { email, password }
+    userLogin(payload).then((res) => {
+      localStorage.setItem("token", res.data.token)
+      localStorage.setItem("user", JSON.stringify(res.data.user))
       navigate('/')
-     
-  }).catch(err=>{
-      if(err.response.data.error){
-      //    console.log(err.response.data.message) 
-         toast.error(err.response.data.error);
+
+    }).catch(err => {
+      if (err.response.data.error) {
+        //    console.log(err.response.data.message) 
+        toast.error(err.response.data.error);
       }
-      console.log(err)}
-  )
-  setLoader(false)
+      console.log(err)
+    }
+    )
+    setLoader(false)
   }
   return (
     <>
       <div className='container-fluid'>
         <div className='row loginmaindiv'>
-          <div className='col-6 backgroundcol'>
-              <div className='loginsidetext'>
-              <span>Don't have </span><br/>
+          <div className='col-7 backgroundcol'>
+            <div className='loginsidetext'>
+              <span>Don't have </span><br />
               <span>an account?</span>
               <div className='btncreate'>
-              Create free account
-              <div className='createbtn'> </div>
+                Create free account
+                <div className='createbtn'> </div>
               </div>
-              </div>
+            </div>
           </div>
-          <div className='col-6 '>
+          <div className='col-5 '>
             <div className='logindiv'>
               <div className='wraper '>
                 <div className='logininputdiv'>
