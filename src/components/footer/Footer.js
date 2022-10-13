86b8ab99acdc27e68157c11feaa4896d76870d6e
saudkhanbpk <saudkhanbpk@gmail.com>
2022-10-13 16:53:49 +0500

    footer fixed

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,35 +3,35 @@ import '../footer/Footer.css'
 export default function Footer() {
   return (
     <>
-      <div className='container-fluid  p-4 bg-light'>
+      <div className='container-fluid p-4 bg-light'>
         <div className='row '>
-          <div className='col-3 offset-1 '>
+          <div className='col-md-4 '>
             <div className='footerinfo'>
               <h4>Tidio</h4>
               <p>Tidio 2022. All rights reserved.</p>
             </div>
             <p className='mt-2'>support@tidio.net</p>
           </div>
-          <div className='col-2'>
+          <div className='col-md-2'>
             <h4>Tidio</h4>
             <p>Affiliate Program</p>
             <p>Contact</p>
             <p>Jobs – We're hiring!</p>
             <p>Privacy Policy</p>
           </div>
-          <div className='col-2'>
+          <div className='col-md-2'>
             <h4>Product</h4>
             <p>Live Chat</p>
             <p>Chatbots</p>
             <p>Pricing</p>
           </div>
-          <div className='col-2'>
+          <div className='col-md-2'>
             <h4>Integration</h4>
             <p>Shopify</p>
             <p>WordPress</p>
             <p>Wix</p>
           </div>
-          <div className='col-2'>
+          <div className='col-md-2'>
             <h4>Resources</h4>
             <p>Blog</p>
             <p>Case Studies</p>
