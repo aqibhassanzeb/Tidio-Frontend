6f7fdb6a303c85aade38c289c90f54fd7dc33146
devshehzadshah <devshehzadshah249@gmail.com>
2022-10-14 16:41:21 +0500

    dasboard

--- src/routescomp/Routespath.js
+++ src/routescomp/Routespath.js
@@ -1,26 +1,38 @@
 import React, { useEffect } from 'react'
-import { Routes, Route,} from "react-router-dom";
+import { Routes, Route, } from "react-router-dom";
 import CustomerServices from '../components/CustomerServices/CustomerServices';
+import Chat from '../components/dashboard/pages/chat/Chat';
+import Contact from '../components/dashboard/pages/contact/Contact';
+import DashboardHeader from '../components/dashboard/pages/dashboardheader/DashboardHeader';
+import Sidebar from '../components/dashboard/pages/sidebar/Sidebar';
+import Footer from '../components/footer/Footer';
+import Header from '../components/Header/Header';
 import Login from '../components/login/Login';
 import Pricing from '../components/pricing/Pricing';
 import Register from '../components/register/Register';
 
 const Routespath = () => {
-    useEffect(() => {
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"))
+  }, [Routespath])
+  return (
+    <>
 
-        const user = JSON.parse(localStorage.getItem("user"))
-      },[Routespath])
- return(
-    <Routes>
-      <Route path="/" exact element={<CustomerServices/>} />
-      <Route path="/userlogin" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-     <Route path="/login" element={<Login />} />
-     <Route path="/pricing" element={<Pricing />} />
+      {false ? <Routes>
+        <Route path="/" exact element={<>   <Header />  <CustomerServices />  <Footer /> </>} />
+        <Route path="/userlogin" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
 
-
-    </Routes>
- )
+        : <Sidebar>
+          <Routes>
+            <Route path="/chat" element={ <> <DashboardHeader /><Chat /> </>}  />
+            <Route path="/contact" element={ <> <DashboardHeader /><Contact /> </>} />
+          </Routes>
+        </Sidebar>}
+    </>
+  )
 }
 
 export default Routespath
