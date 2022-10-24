25b5baaf61338f2ba45b3c9aeb836ffe15ea074a
Aqib Hassan Zeb <aqibhassanzeb@gmail.com>
2022-10-24 17:05:52 +0500

    socail chat live

--- src/apis/Chat-api.js
+++ src/apis/Chat-api.js
@@ -3,39 +3,39 @@ import { apiURL } from '../config/index'
 
 
 let usertoken = localStorage.getItem("token")
-const config={
-    headers:{
-        
-        Authorization:`Bearer ${usertoken}`
+const config = {
+    headers: {
+
+        Authorization: `Bearer ${usertoken}`
     }
 }
-const config2={
-    headers:{
-        Authorization:`Bearer ${usertoken}`
+const config2 = {
+    headers: {
+        Authorization: `Bearer ${usertoken}`
     }
 }
 
 
 
-export const getAllUser=async(reqParam) => {
-    const response  = await apiURL.get(`alluser?search=${reqParam}`,config2);
-        return response
-      }
-      
-      
-      
-export const selectUser=async(reqParam) => {
-    const response  = await apiURL.post('chat',reqParam,config);
-        return response
-      }
-      
-export const sendMessage=async(reqParam) => {
-      const response  = await apiURL.post('messages',reqParam,config);
-          return response
-               }
-export const fetchMessages=async(reqParam) => {
-      const response  = await apiURL.get(`messages/${reqParam}`,config);
-          return response
-               }
+export const getAllUser = async (reqParam) => {
+    const response = await apiURL.get(`alluser?search=${reqParam}`, config2);
+    return response
+}
+
+
+
+export const selectUser = async (reqParam) => {
+    const response = await apiURL.post('chat', reqParam, config);
+    return response
+}
+
+export const sendMessage = async (reqParam) => {
+    const response = await apiURL.post('messages', reqParam, config);
+    return response
+}
+export const fetchMessages = async (reqParam) => {
+    const response = await apiURL.get(`messages/${reqParam}`, config);
+    return response
+}
 
 
--- src/components/chatinbox/ChatInbox.js
+++ src/components/chatinbox/ChatInbox.js
@@ -3,16 +3,16 @@ import "../chatinbox/ChatInbox.css"
 import melissa from '../../images/melissa-j.webp'
 import { fetchMessages, sendMessage } from '../../apis/Chat-api'
 import { useDispatch, useSelector } from 'react-redux'
-
-import { io } from 'socket.io-client'
+import  io  from 'socket.io-client'
 import { setNotification } from '../../redux/features/ChatSlice'
 
-var ENDPOINT =process.env.REACT_APP_SOCKET_LINK
-var socket
+
+var ENDPOINT =  process.env.REACT_APP_SOCKET_LINK
+var socket =io() 
 var selectedChatCompare
 
 
-function ChatInbox({senderUser}) {
+function ChatInbox({ senderUser }) {
     const [messages, setMessages] = useState([])
     const [newmessage, setNewmessage] = useState('')
     const [loading, setLoading] = useState(false)
@@ -20,147 +20,150 @@ function ChatInbox({senderUser}) {
     const [typing, setTyping] = useState(false)
     const [isTyping, setIsTyping] = useState(false)
 
-    console.log("procesdkljfsldfj",process.env.REACT_APP_SOCKET_LINK)
 
-    const selectedUser=useSelector(state=> state.SelectedUser.selectedUser)
-    const loginUser=useSelector(state=> state.User.activeUser)
-    const notification=useSelector(state=> state.SelectedUser.notification)
-   const dispatch= useDispatch()
+    const selectedUser = useSelector(state => state.SelectedUser.selectedUser)
+    const loginUser = useSelector(state => state.User.activeUser)
+    const notification = useSelector(state => state.SelectedUser.notification)
+    const dispatch = useDispatch()
 
     // console.log("messages :",messages);
 
-    const sendMessageHandle=(e)=>{
-        const content=newmessage
-        socket.emit("stop typing", selectedUser._id)
-        const chatId=selectedUser._id
-        const payload={content,chatId}
+    const sendMessageHandle = (e) => {
+        const content = newmessage
+        // socket.emit("stop typing", selectedUser._id)
+        const chatId = selectedUser._id
+        const payload = { content, chatId }
         setLoading(true)
-        sendMessage(payload).then((res)=>{
+        sendMessage(payload).then((res) => {
             console.log(res.data)
-            const data=res.data
-            setMessages([...messages,data])
+            const data = res.data
+            setMessages([...messages, data])
             setNewmessage("")
             setLoading(false)
-            socket.emit("new message",data && data)
-        }).catch((err)=>{
+            socket.emit("new message", data && data)
+        }).catch((err) => {
             setLoading(false)
-            console.log(err)})
+            console.log(err)
+        })
     }
 
-    const fetchMessageshandle=()=>{
-        const chatId=selectedUser._id
-        fetchMessages(chatId).then((res)=>{
-
-            console.log(res.data);
+    const fetchMessageshandle = () => {
+        const chatId = selectedUser._id
+        fetchMessages(chatId).then((res) => {
             setMessages(res.data)
-            socket.emit("join chat",chatId && chatId)
-        }).catch(err=>{console.log(err);})
+            socket.emit("join chat", chatId && chatId)
+        }).catch(err => { console.log(err); })
     }
 
 
     useEffect(() => {
-        socket=io(ENDPOINT)
-        socket.emit("setup",loginUser !== null && loginUser);
-        socket.on("connected",()=>setSocketConnected(true))
-        socket.on("typing",()=>setTyping(true))
-        socket.on("stop typing",()=>setIsTyping(false))
-      }, [])
+        // socket.on("connected", () => setSocketConnected(true))
+        socket = io(ENDPOINT)
+        socket.emit("setup", loginUser !== null && loginUser);
+        socket.on("connected", () => setSocketConnected(true))
+        // socket.on("typing", () => setIsTyping(true))
+        // socket.on("stop typing", () => setIsTyping(false))
+    })
+    // useEffect(() => {
+    // })
+    
 
-    const handleChange=()=>{
-        if(!socketConnected) return;
-        if(!typing){
+    const handleChange = () => {
+        socket.emit("typing", selectedUser && selectedUser._id)
+        if (!socketConnected) return;
+        if (!typing) {
             setTyping(true)
-            socket.emit("typing",selectedUser && selectedUser._id)
         }
-        let lastTypingTime= new Date().getTime();
-        var timerlength=3000;
+        let lastTypingTime = new Date().getTime();
+        var timerlength = 3000;
         setTimeout(() => {
-            var timeNow =new Date().getTime();
-            var timeDiff=timeNow-lastTypingTime
-            if(timeDiff>= timerlength && typing){
-                socket.emit("stop typing",selectedUser && selectedUser._id)
+            var timeNow = new Date().getTime();
+            var timeDiff = timeNow - lastTypingTime
+            if (timeDiff >= timerlength && typing) {
+                socket.emit("stop typing", selectedUser && selectedUser._id)
                 setTyping(false)
             }
         }, timerlength);
     }
 
-    
-
-useEffect(() => {
- selectedUser && fetchMessageshandle()
-  selectedChatCompare = selectedUser ? selectedUser : null
-
-}, [selectedUser])
 
 
     useEffect(() => {
-        console.log("use effect ...");
-     socket.on("message recieved",(newMessageRecieved)=>{
-        if(!selectedChatCompare || selectedChatCompare._id !== newMessageRecieved.chat._id){
-            console.log("if portion ,;;")
-            // if(!notification.includes(newMessageRecieved)){
+        selectedUser != null && fetchMessageshandle();
+        selectedChatCompare = selectedUser ? selectedUser :null
+    },[selectedUser])
 
-            //     dispatch(setNotification(newMessageRecieved))
-            // }
-        }else{
-            setMessages([...messages,newMessageRecieved])
-        }
-     })
+
+
+
+
+    useEffect(() => {
+        socket.on("messagerecieved", (newMessageRecieved) => {
+            if (!selectedChatCompare || selectedChatCompare._id !== newMessageRecieved.chat._id) {
+                console.log("if portion ,;;")
+                // if(!notification.includes(newMessageRecieved)){
+
+                //     dispatch(setNotification(newMessageRecieved))
+                // }
+            } else {
+                setMessages([...messages, newMessageRecieved])
+            }
+        })
     })
-    
+
 
     return (
         <>
             <div className='container-fluid custom_fluid'>
                 <div className='row mt-4'>
                     <div className='col-sm-12 header_chat'>
-                   <div className='display_header'>
-                   <img className='chat_icon' src={senderUser ? senderUser.imageUrl:melissa} /> 
-                    <p className='online'>{senderUser.name}</p>
-                   </div>
+                        <div className='display_header'>
+                            <img className='chat_icon' src={senderUser ? senderUser.imageUrl : melissa} />
+                            <p className='online'>{senderUser.name}</p>
+                        </div>
                     </div>
                 </div>
-               { selectedUser ? <div className='row chat-area' >
-                   {
-               messages &&    messages.map((elm,index)=>{
-                    var setDate = new Date(elm.createdAt) 
+                {selectedUser ? <div className='row chat-area' >
+                    {
+                        messages && messages.map((elm, index) => {
+                            var setDate = new Date(elm.createdAt)
 
-                return(
+                            return (
 
-                      elm.sender._id !== loginUser._id?   <div className='col-sm-12 text-left p-2'>
-                        <div className='chat_row'>
-                            <img className='chat_icon' src={elm.imageUrl} /> 
-                            <h5 className='chat_name'>{elm.name}</h5> 
-                            <time className='chat_time'>{ setDate ? setDate.toLocaleTimeString('en-US'):"N/A"}</time>
-                                                
-                        </div>
-                        <div className='chat_text'>
-                            <p>{elm.content}</p>
-                        </div>
-                    </div>:
-                    <div className='col-sm-12 text-end  p-2'>
-                        <div className='chat_end_row'>
-                            <time className='chat_end_time'>{ setDate ? setDate.toLocaleTimeString('en-US'):"N/A"}</time>
-                            <h5 className='chat_end_name'>{elm.name}</h5>
-                            <img className='chat_end_icon'src={elm.imageUrl} />
-                        </div>
-                        <div className='chat_end_text'>
-                            <p>{elm.content}</p>
-                        </div>
-                    </div>
-                        )
-                    })
-                }
-                </div>:<h2>Empty</h2> }
+                                elm.sender._id !== loginUser._id ? <div className='col-sm-12 text-left p-2'>
+                                    <div className='chat_row'>
+                                        <img className='chat_icon' src={elm.imageUrl} />
+                                        <h5 className='chat_name'>{elm.name}</h5>
+                                        <time className='chat_time'>{setDate ? setDate.toLocaleTimeString('en-US') : "N/A"}</time>
+
+                                    </div>
+                                    <div className='chat_text'>
+                                        <p>{elm.content}</p>
+                                    </div>
+                                </div> :
+                                    <div className='col-sm-12 text-end  p-2'>
+                                        <div className='chat_end_row'>
+                                            <time className='chat_end_time'>{setDate ? setDate.toLocaleTimeString('en-US') : "N/A"}</time>
+                                            <h5 className='chat_end_name'>{elm.name}</h5>
+                                            <img className='chat_end_icon' src={elm.imageUrl} />
+                                        </div>
+                                        <div className='chat_end_text'>
+                                            <p>{elm.content}</p>
+                                        </div>
+                                    </div>
+                            )
+                        })
+                    }
+                </div> : <h2>Empty</h2>}
                 <div className='row pb-4  position_textarea'>
-                        
-                {/* {isTyping ?<p>Typing...</p>:""} */}
+
+                    {/* {isTyping ?<p>Typing...</p>:""} */}
                     <div className='col-sm-12 text-end'>
                         {/* <input type='file' /> */}
-                  { loading ? <p>loading..</p> : selectedUser  &&  <button className='btn btn-primary' onClick={sendMessageHandle} >Reply</button>}
+                        {loading ? <p>loading..</p> : selectedUser && <button className='btn btn-primary' onClick={() => {sendMessageHandle()}} >Reply</button>}
                     </div>
                     <div className='col-sm-12 text_area_padding'>
-                        <textarea className='form-control custom_text_area' value={newmessage} onChange={(e)=>{setNewmessage(e.target.value);handleChange()}}>
+                        <textarea className='form-control custom_text_area' value={newmessage} onChange={(e) => { setNewmessage(e.target.value) }}>
                         </textarea>
                     </div>
                 </div>
