97a6e37c1500ff2dcb14a59715a65e70ba492e47
saudkhanbpk <saudkhanbpk@gmail.com>
2022-11-07 16:55:35 +0500

    forget ui done

--- src/components/CustomerServices/CustomerServices.jsx
+++ src/components/CustomerServices/CustomerServices.jsx
@@ -18,6 +18,9 @@ import PricingModel from '../pricingmodel/PricingModel';
 import Footer from './../footer/Footer';
 import { useNavigate } from 'react-router-dom';
 import SimpleInstallation from '../simpleInstalation/SimpleInstallation';
+import { RiSendPlaneFill } from 'react-icons/ri';
+import { FaForumbee, FaShopify, FaWordpress } from 'react-icons/fa';
+import { BsGoogle } from 'react-icons/bs';
 
 function CustomerServices() {
 
@@ -146,6 +149,18 @@ function CustomerServices() {
                     <div className='col-sm-4  mt-4 imgsldie'>
                         <img className='imgview img-fluid' src={backgroundview} />
 
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='mt-5'>
+                        <h1 className='text-center'>Excellence confirmed by 1961+ reviews</h1>
+                    </div>
+                    <div className='excellenceicon'> 
+                   <div className='icondif'><RiSendPlaneFill /> Capterra</div>
+                   <div className='icondif'> <FaShopify /> Shopify</div>
+                   <div className='icondif'><FaWordpress /> Wordpress</div>
+                   <div className='icondif'> <BsGoogle /> Google</div>
+                   <div className='icondif'><FaForumbee /> GetApp</div>
                     </div>
                 </div>
                 {/* card section */}
@@ -305,7 +320,7 @@ function CustomerServices() {
                     </div>
                 </div>
                 <div className='container'>
-                    <div className='row'>
+                    <div className='row rowreverse'>
                         <div className='col-sm-12 col-md-6 live_chat_padding'>
                             <img className='img img-fluid' src={Ai} />
                         </div>
@@ -339,7 +354,7 @@ function CustomerServices() {
                     </div>
                 </div>
                 <div className='container'>
-                    <div className='row'>
+                    <div className='row rowreverse'>
                         <div className='col-sm-12 col-md-6 live_chat_padding'>
                             <img className='img img-fluid' src={messenger} />
                         </div>
@@ -374,25 +389,12 @@ function CustomerServices() {
                 </div>
 
 
+                
                 <div className='container'>
-                    <div className='row'>
-                        <div className='col-sm-12 col-md-6 live_chat_padding'>
-                            <img className='img img-fluid' src={messenger} />
+                    <div className='row rowreverse live_chat_padding'>
+                    <div className='col-sm-12 col-md-6 img_live_chat_padd'>
+                            <img className='img img-fluid' src={closed} />
                         </div>
-                        <div className='col-sm-12 col-md-6'>
-                            <h3 className='live_chat virtual_padd'>Answer any channel  from one place</h3>
-                            <p className='half_of'>As many as 75% of online customers said they expect help within five minutes. Connect all your channels to Tidio and answer your customers in an instant!</p>
-                            <ul className='ul_padding'>
-                                <li className='connect_li'>Integrate your live chat, email inbox,  Messenger, and Instagram into one  multichannel communicator</li>
-                                <li className='connect_li'>Talk to your customers on all devices:  desktop, mobile, and tablet</li>
-                                <li className='connect_li'>Never lose a potential customer because  they contacted you on a channel you  rarely use</li>
-                            </ul>
-                            <a className='discover_live'>Discover Tidio Chatbots</a><i className="bi bi-arrow-right-short"></i>
-                        </div>
-                    </div>
-                </div>
-                <div className='container'>
-                    <div className='row live_chat_padding'>
                         <div className='col-sm-12 col-md-6 liv_col'>
                             <h3 className='live_chat more_sales'>Empower your  customer support  team</h3>
                             <p className='half_of'>Did you know that 3M businesses  generated 20M leads and $400M in sales after they started using our  chatbots?</p>
@@ -403,9 +405,7 @@ function CustomerServices() {
                             </ul>
                             <a className='discover_live'>try for free now!</a><i className="bi bi-arrow-right-short"></i>
                         </div>
-                        <div className='col-sm-12 col-md-6 img_live_chat_padd'>
-                            <img className='img img-fluid' src={closed} />
-                        </div>
+                        
                     </div>
                 </div>
                 <SimpleInstallation/>
--- src/components/resetpassword/ResetPass.jsx
+++ src/components/resetpassword/ResetPass.jsx
@@ -3,56 +3,63 @@ import { useNavigate } from 'react-router-dom'
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { resetPassword } from '../../apis/Auth-api';
-
+import './ResetPassword.css'
 const ResetPass = () => {
-    const [email, setEmail] = useState("")
-    const [loader, setLoader] = useState(false)
-    const navigate = useNavigate()
-  
-  
-    const submitHandle=(e)=>{
-        e.preventDefault()
-        setLoader(true)
-        if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
-            toast.error("Invalid email !");
-            setLoader(false)
-            return
-        }
-        const payload= {email}
-        resetPassword(payload).then((res) => {
-                console.log("responce :",res.data.message)
-                // toast.success(res.data.message)
-                alert("Check your email for password reset link")
-            navigate('/userlogin')
-           
-        }).catch(err=>{
-          if(err.response.data.error){
-            toast.error(err.response.data.error);
-            setLoader(false)
-         }
-            console.log(err)
-            setLoader(false)
-        }
-        )
+  const [email, setEmail] = useState("")
+  const [loader, setLoader] = useState(false)
+  const navigate = useNavigate()
+
+
+  const submitHandle = (e) => {
+    e.preventDefault()
+    setLoader(true)
+    if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+      toast.error("Invalid email !");
+      setLoader(false)
+      return
     }
+    const payload = { email }
+    resetPassword(payload).then((res) => {
+      console.log("responce :", res.data.message)
+      // toast.success(res.data.message)
+      alert("Check your email for password reset link")
+      navigate('/userlogin')
+
+    }).catch(err => {
+      if (err.response.data.error) {
+        toast.error(err.response.data.error);
+        setLoader(false)
+      }
+      console.log(err)
+      setLoader(false)
+    }
+    )
+  }
   return (
     <div className='container-fluid'>
-        <ToastContainer />
+      <ToastContainer />
+      <div className='row'>
+      <div className='col'>
         <div className='d-flex justify-content-center mt-5'>
 
-        <h1>Forgot Password</h1>
+          <h1>Forgot Password</h1>
         </div>
-      <div className='d-flex justify-content-center mt-5 mb-5'>
 
-         <form onSubmit={submitHandle}>
+        <div className=' inputforget mt-5 mb-5'>
 
-        <input onChange={(e)=>setEmail(e.target.value)} className="form-control" placeholder="Email..." />
-      { loader ? <button className='btn btn-primary float-end'>loading..</button>
-      : 
-      <button className='btn btn-primary float-end' type='submit'>Submit</button>}
-      </form>
+          <form onSubmit={submitHandle}>
+
+            <input onChange={(e) => setEmail(e.target.value)} className="form-control " placeholder="Email..." />
+            {loader ? <button className='btn btn-primary float-end'>loading..</button>
+              :
+              <div className=' d-flex justify-content-center'>
+              <button className='subbtn mt-3' type='submit'>Submit</button>
+              </div>}
+          </form>
+        </div>
       </div>
-      </div>
+    </div>
+    </div>
   )
 }
 
