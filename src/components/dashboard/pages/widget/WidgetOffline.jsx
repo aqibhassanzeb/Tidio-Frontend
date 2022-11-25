6830f70c7c1dd109e1bb3c6bb1aa7ba3073cbedb
Aqib Hassan Zeb <aqibhassanzeb@gmail.com>
2022-11-25 15:10:25 +0500

    updated

--- src/components/dashboard/pages/widget/WidgetOffline.jsx
+++ src/components/dashboard/pages/widget/WidgetOffline.jsx
@@ -4,18 +4,21 @@ import { BsThreeDotsVertical } from 'react-icons/bs';
 import { IoIosClose } from 'react-icons/io';
 import { BsArrowDownRight } from 'react-icons/bs';
 import userim from "../../../../images/horse.jpg"
-const WidgetOffline = () => {
+const WidgetOffline = ({getStarted}) => {
     return (
         <>
             <div className=' bg-light'>
-            <div className='d-flex justify-content-end'> <IoIosClose style={{fontSize: "30px",cursor: "pointer"}} /> </div>
+            <div className='d-flex justify-content-end'> </div>
                 <div className='d-flex justify-content-center'>
                     <img src={userim} className=' hrim' alt='' />
                    
                 </div>
                 <div>
-                    <p className='apraforofline'>We’re currently unavailable. We’ll get back to you when one of our operators is able to respond.
-                     Please provide your email address so we can get in touch with you.</p>
+                    <p className='apraforofline'>
+                        {/* We’re currently unavailable. We’ll get back to you when one of our operators is able to respond.
+                     Please provide your email address so we can get in touch with you. */}
+                     {getStarted?.offlineMessage}
+                     </p>
                      
                 </div>
                 <div className='transdata '>
